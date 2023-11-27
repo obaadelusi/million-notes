d373d109321e4edd4a12c480b252b4a7369bdf77
Oba <adelusiobafunsho@gmail.com>
2023-11-26 21:43:49 -0600

    error redirect setup

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php include('header.php') ?>
+
+<main class="error-page">
+    <div class="error-page-container">
+        <h1>Page Not Found</h1>
+        <h2>Error 404</h2>
+        <p><?=$_SERVER['REQUEST_URI']?> does not exist.</p>
+    </div>
+</main>
+
+<?php include('footer.php') ?>
