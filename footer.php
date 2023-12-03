9b6143bf058df648b8c740430ea1cf5da188430b
Oba <adelusiobafunsho@gmail.com>
2023-12-03 13:14:02 -0600

    footer content & styles

--- footer.php
+++ footer.php
@@ -1,5 +1,11 @@
 <footer class="footer">
-    <small class="d-block text-end container">Million Notes Book Club &copy; <?=date('Y')?> • Built by Oba</small>
+    <div class="container footer-container">
+        <div>
+            <div class="logo">Million<span>Notes</span></div>
+            <small>Book Club &copy; <?=date('Y')?> • Built by Oba</small>
+        </div>
+        <a href="https://github.com/obaadelusi/million-notes" target="_blank" class="btn-outline-primary footer-btn">GitHub</a>
+    </div>
 </footer>
 
 </body>
