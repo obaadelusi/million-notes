1a958095e8c4a82ab5c1b260474171fda5dcc284
Oba <adelusiobafunsho@gmail.com>
2023-12-03 13:42:03 -0600

    icon add

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 
 <main class="error-page">
     <div class="error-page-container">
-        <h1>Page Not Found</h1>
+        <h1>🚨 Page Not Found</h1>
         <h2>Error 404</h2>
         <p><?=$_SERVER['REQUEST_URI']?> does not exist.</p>
     </div>
