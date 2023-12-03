537425bc1487feacc2604e2c062d56e462f397f7
Oba <adelusiobafunsho@gmail.com>
2023-12-03 13:14:58 -0600

    home content and styling

--- index.php
+++ index.php
@@ -23,50 +23,63 @@ $statement->execute();
 
 <?php include('header.php'); ?>
 
-<main class="main container">
+<main class="main">
     <section class="hero">
-        <div class="hero-content">
-            <small>⭐ <?=date("F")?> Read</small>
-            <h1>Million Dollar Habits</h1>
-            <p>Proven Power Practices to Double & Triple Your Income</p>
-            <p><em>by</em>&ensp;Brian Tracy</p>
+        <div class="container hero-container">
+            <div class="hero-content">
+                <small>📖 Book Club</small>
+                <h1>Get & Make Summaries <br>of Your Favourite Books.</h1>
+                <p>🖊 Read. Review. Make notes. Monthly.</p>
 
-            <div class="hero-cta">
-                <a href="m-notes.php?book_id=1" class="btn-primary btn-lg">Read BookNotes</a>
-                <a href="#" class="btn-link">View Book</a>
+                <div class="hero-cta">
+                    <a href="m-notes.php?book_id=1" class="btn-primary btn-lg">Join Club</a>
+                    <a href="#" class="btn-link">View Monthly Reads</a>
+                </div>
+            </div>
+            <div class="hero-image">
+                <img src="images/hero_books_image.png" alt="million dollar habit">
             </div>
         </div>
-        <div class="hero-image">
-            <img src="uploads/million_dollar_habits_brian_tracy.png" alt="million dollar habit">
+    </section>
+
+    <section class="monthread container section-padding">
+        <div class="monthread-card ">
+            <div class="monthread-content">
+                <small>⭐ This Month's Read</small>
+                <h3>Million Dollar Habits</h3>
+                <p>Proven Power Practices to Double & Triple Your Income</p>
+                <p><em>by</em>&ensp;Brian Tracy</p>
+                <div class="monthread-cta">
+                    <a href="m-notes.php?book_id=1" class="btn-outline-primary">Read BookNotes -></a>
+                </div>
+            </div>
+            <div class="monthread-image">
+                <img src="uploads/million_dollar_habits_brian_tracy.png" alt="million dollar habit">
+            </div>
+        </div>
+        <div class="monthread-sugesstioncard">
+            <div class="monthread-content">
+                <small>⏭ Next Month - <?=(new \DateTime('next month'))->format('F Y')?></small>
+                <h3>25 Book Suggestions</h3>
+                <p>Top Suggestion: 🥇 Becoming <em>by</em>&ensp;Michelle Obama</p>
+                <p></p>
+                <div class="monthread-cta">
+                    <a href="m-notes.php?book_id=1" class="btn-link">View Suggestions -></a>
+                </div>
+            </div>
         </div>
     </section>
-    
-    <?php if($statement->rowCount() == 0) : ?>
-        <div class="text-center py-1">
-            <p>No blog entries yet!</p>
-            <a href="#" class="button-primary">+ New Blog</a>
-        </div>
-    <?php exit; endif; ?>
 
-    <!-- Fetch each table row in turn. Each $row is a table row hash.
-    Fetch returns FALSE when out of rows, halting the loop. -->
-    <?php while($row = $statement->fetch()): ?>
-        <article class="blog-post">
-            <h3 class="blog-post-title">
-                <a href="show.php?id=<?=$row['id']?>"><?=$row['title']?></a>
-            </h3>
-            <small class="blog-post-date">
-                Posted on <time datetime="<?=$row['date_posted']?>"><?=date_format(date_create($row['date_posted']), 'F j, Y G:i') ?><time>
-                &ensp; <a href="edit.php?id=<?=$row['id']?>" class="blog-post-edit">edit</a>
-            </small>
-            <p class="blog-post-content">
-                <?=substr(strip_tags(html_entity_decode($row['content'])), 0, 200)?>
-                <?php if(strlen($row['content']) > 200) : ?>
-                    ... <a href="show.php?id=<?=$row['id']?>">Read more</a>
-                <?php endif ?>
-            </p>
-        </article>
-    <?php endwhile; ?>
+    <section class="indexcta section-padding">
+        <h2>📩 Get Invited</h2>
+        <form action="#" class="form">
+            <div class="form-control">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email">
+            </div>
+            <button class="btn-primary">Join Club</button>
+        </form>
+    </section>
 
 </main>
 
